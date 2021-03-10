f9b9ef813a7d4724f19eaa52449e6b52e3220f46
Your Name <brettjames250@gmail.com>
2021-03-10 08:52:20 +0000

    updating messages.js

---
+++ messages.js
@@ -0,0 +1,16 @@
+
+const firstPart = ['You are ', 'I am ', 'We are ', 'Boris Johnson is ', 'Pamela Anderson is ']
+const secondPart = ['exceptionally ', 'slightly ', 'kind of ' , 'possibly ']
+const thirdPart = ['strange', 'interesting', 'peculiar', 'great', 'tired', 'ripped']
+
+
+const chooseWordPart = array => { 
+ return array[Math.floor(Math.random() * array.length)]
+}
+
+
+const generateMessage = () => {
+    return chooseWordPart(firstPart) + chooseWordPart(secondPart) + chooseWordPart(thirdPart) 
+}
+
+console.log(generateMessage())
