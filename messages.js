ca003a05fce981c9b8f8fcffa8f51838e4e12755
Your Name <brettjames250@gmail.com>
2021-03-10 10:17:10 +0000

    reformatting

--- messages.js
+++ messages.js
@@ -3,11 +3,9 @@ const firstPart = ['You are ', 'I am ', 'We are ', 'Boris Johnson is ', 'Pamela 
 const secondPart = ['exceptionally ', 'slightly ', 'kind of ' , 'possibly ']
 const thirdPart = ['strange', 'interesting', 'peculiar', 'great', 'tired', 'ripped']
 
-
 const chooseWordPart = array => { 
  return array[Math.floor(Math.random() * array.length)]
 }
-
 
 const generateMessage = () => {
     return chooseWordPart(firstPart) + chooseWordPart(secondPart) + chooseWordPart(thirdPart) 
